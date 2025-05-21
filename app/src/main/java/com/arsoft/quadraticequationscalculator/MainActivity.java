9d283c816e367e761b978d035ca4f21852ea29b8
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-21 20:37:09 +0600

    Project completed without follow the MVVM architecture

--- app/src/main/java/com/arsoft/quadraticequationscalculator/MainActivity.java
+++ app/src/main/java/com/arsoft/quadraticequationscalculator/MainActivity.java
@@ -7,18 +7,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.arsoft.quadraticequationscalculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ActivityMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        mainBinding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_main
+        );
+
+        MyEquation equation = new MyEquation(mainBinding);
+
+        mainBinding.setMyEquation(equation);
+
+
+
     }
 }
